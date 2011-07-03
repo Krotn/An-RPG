5c8bfd7151487f8fe4158d547f0f4c91badb6313
Karl Otness <karl@otness.com>
2011-07-03 17:20:53 -0500

    GameItem now contains whether or not it is stackable.

--- basic.py
+++ basic.py
@@ -3,5 +3,6 @@ class GameEntity:
         self.name = name
 
 class GameItem(GameEntity):
-    def __init__(name):
+    def __init__(name,stackable = True):
         super(GameItem,self).__init__(name = name)
+        self.stackable = stackable
